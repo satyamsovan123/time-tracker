cc6e3fbb8bb8ea73e5982c7eb25ea74d326ae8a2
Satyam Sovan Mishra <msatyamsovan@gmail.com>
2022-11-09 17:51:56 +0530

    Add base files

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT);
