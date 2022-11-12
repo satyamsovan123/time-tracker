942535e9b298d40d0c6d94ff91838df6796817b1
Satyam Sovan Mishra <msatyamsovan@gmail.com>
2022-11-12 09:31:50 +0530

    A little housekeeping

--- configs/connectToDatabase.js
+++ configs/connectToDatabase.js
@@ -10,8 +10,25 @@ const { logger } = require("../app/utils/logger");
  * it logs the status after connection
  */
 function connectToDatabase() {
+  /**
+   * This is URL fetched from environment file
+   *
+   * @type {string}
+   * @const
+   */
   const uri = process.env.DB_URL;
+
+  /**
+   * This is options object that needs to passed while connecting to MongoDB
+   *
+   * @type {}
+   * @const
+   */
   const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+  /**
+   * This is the connection function
+   */
   mongoose.connect(uri, options).then(
     () => {
       logger(commonConstant.DB_CONNECTION_STATUS_OK);
