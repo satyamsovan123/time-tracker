a80f93ad908036a1f9cca98b5666870f9c6af6b1
Satyam Sovan Mishra <msatyamsovan@gmail.com>
2022-11-24 01:27:11 +0530

    Testing cookies, frontend deployment #32

--- app/routes/index.js
+++ app/routes/index.js
@@ -14,7 +14,7 @@ router.use(baseURL, require("./insight"));
  * This middleware function is handling the base route for it's used for quick testing and welcome
  */
 router.get("/", (req, res) => {
-  console.log(req.cookie[BODY_CONSTANT.TIME_TRACKER_TOKEN]);
+  console.log(req.cookies[BODY_CONSTANT.TIME_TRACKER_TOKEN]);
 
   /**
    * This is the response that is sent to client
