d0640ada98dfce0e2eec3b4cdba194c2f08a0da6
Satyam Sovan Mishra <msatyamsovan@gmail.com>
2022-11-23 16:49:15 +0530

    Testing deployment

--- app.js
+++ app.js
@@ -7,8 +7,16 @@ const initializeDatabase = require("./configs/connectToDatabase.js");
 const routes = require("./app/routes");
 const { validateRequestJSON } = require("./app/utils/validateRequestJSON.js");
 
-app.use(cors());
-// app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
+// app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      process.env.FRONTEND_DEVELOPMENT_URL,
+      process.env.FRONTEND_PRODUCTION_URL,
+    ],
+  })
+);
 app.use(express.json());
 app.use(validateRequestJSON);
 app.use(routes);
