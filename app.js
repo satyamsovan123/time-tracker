65df8d3e5d677d0f6ebad08c5b0b295e319b664b
Satyam Sovan Mishra <msatyamsovan@gmail.com>
2022-11-23 20:07:22 +0530

    Testing CORS, frontend deployment #13

--- app.js
+++ app.js
@@ -8,27 +8,27 @@ const routes = require("./app/routes");
 const { validateRequestJSON } = require("./app/utils/validateRequestJSON.js");
 
 // app.use(cors());
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_PRODUCTION_URL,
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: process.env.FRONTEND_PRODUCTION_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // origin: process.env.NODE_ENV === process.env.FRONTEND_PRODUCTION_URL
 //   ? process.env.FRONTEND_PRODUCTION_URL
 //   : process.env.FRONTEND_DEVELOPMENT_URL,
 
-app.use(function (req, res, next) {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://time-tracker-ui.web.app"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://time-tracker-ui.web.app"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE");
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   next();
+// });
 
 app.use(express.json());
 app.use(validateRequestJSON);
