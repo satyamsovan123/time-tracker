2deaef39026a19e480850ab90ccae418da3477bd
Satyam Sovan Mishra <msatyamsovan@gmail.com>
2022-11-23 22:07:51 +0530

    Testing CORS, frontend deployment #14

--- app.js
+++ app.js
@@ -10,15 +10,14 @@ const { validateRequestJSON } = require("./app/utils/validateRequestJSON.js");
 // app.use(cors());
 app.use(
   cors({
-    origin: process.env.FRONTEND_PRODUCTION_URL,
+    origin:
+      process.env.NODE_ENV === process.env.FRONTEND_PRODUCTION_URL
+        ? process.env.FRONTEND_PRODUCTION_URL
+        : process.env.FRONTEND_DEVELOPMENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
-
-// origin: process.env.NODE_ENV === process.env.FRONTEND_PRODUCTION_URL
-//   ? process.env.FRONTEND_PRODUCTION_URL
-//   : process.env.FRONTEND_DEVELOPMENT_URL,
 
 // app.use(function (req, res, next) {
 //   res.setHeader(
