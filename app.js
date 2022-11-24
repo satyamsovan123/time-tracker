655fbc4d0922e4a5f0311c21071f2a79ee09795c
Satyam Sovan Mishra <msatyamsovan@gmail.com>
2022-11-24 13:59:15 +0530

    Access cookie, frontend deployment #40

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use(
     origin: ["https://time-tracker-ui.web.app", "http://localhost:4200"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
+    exposedHeaders: ["cookie", "set-cookie"],
   })
 );
 
